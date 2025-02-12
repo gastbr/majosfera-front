9218192b7ffb07de7c963998ef7f6376415ce50c
gastbr <71907959+gastbr@users.noreply.github.com>
2025-02-12 04:09:17 +0000

    TEST image fix on deploy

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,8 @@ function App() {
   return (
     <div className="flex flex-col gap-20 items-center justify-center bg-amber-800/50 h-screen">
       <div className="flex gap-40">
-        <img className="w-48" src="./public/logo-light-nobg.png" alt="Logo" />
-        <img className="w-48" src="./public/logo-dark-nobg.png" alt="Logo" />
+        <img className="w-48" src="/logo-light-nobg.png" alt="Logo" />
+        <img className="w-48" src="public/logo-dark-nobg.png" alt="Logo" />
       </div>
       <div className="flex flex-col items-center gap-8 bg-amber-700/70 p-12 rounded-3xl">
         <p className="text-6xl text-stone-800 font-bold tracking-widest">oli 🌊</p>
