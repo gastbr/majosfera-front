c818fe737e7ec281c640e3110fa8303672f6c52a
gastbr <71907959+gastbr@users.noreply.github.com>
2025-02-12 04:02:00 +0000

    ADDED logos to index

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
 function App() {
   return (
-    <div className="flex flex-col gap-12 items-center justify-center bg-amber-700 h-screen">
-      <p className="text-6xl text-white font-bold tracking-widest">oli 🌊</p>
-      <p className="text-xs text-stone-300">tailwind funciona</p>
-      <img src=".public/logo-notext.png" alt="Logo" />
+    <div className="flex flex-col gap-20 items-center justify-center bg-amber-800/50 h-screen">
+      <div className="flex gap-40">
+        <img className="w-48" src="./public/logo-light-nobg.png" alt="Logo" />
+        <img className="w-48" src="./public/logo-dark-nobg.png" alt="Logo" />
+      </div>
+      <div className="flex flex-col items-center gap-8 bg-amber-700/70 p-12 rounded-3xl">
+        <p className="text-6xl text-stone-800 font-bold tracking-widest">oli 🌊</p>
+        <p className="text-xs text-stone-700">tailwind funciona</p>
+      </div>
     </div>
   )
 }
