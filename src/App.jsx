46fe593371807e217f2f9894ba51d8d77076e114
gastbr <71907959+gastbr@users.noreply.github.com>
2025-02-12 01:19:38 +0000

    ADDED branches

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,9 @@
-import './App.css'
-
 function App() {
   return (
-    <>
-      <p>Hola</p>
-    </>
+    <div className="flex flex-col gap-12 items-center justify-center bg-amber-700 h-screen">
+      <p className="text-6xl text-white font-bold tracking-widest">oli 🌊</p>
+      <p className="text-xs text-stone-300">tailwind funciona</p>
+    </div>
   )
 }
 
