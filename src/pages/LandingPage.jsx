f3d925e819775e81da6b9b8fd7166ad5cd579f52
AlumG <AlumG@majada.es>
2025-02-18 21:52:01 +0100

    FIX app URL adapted

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,6 +1,9 @@
 import { Link } from "react-router";
 
 const LandingPage = () => {
+
+  const backURL = import.meta.env.VITE_API_URL;
+
   return (
     <div className="min-h-screen flex flex-col bg-amber-100 text-amber-900">
       {/* Header */}
@@ -91,13 +94,13 @@ const LandingPage = () => {
         {/* Sección de Login y Registro */}
         <div className="flex space-x-4 mt-6">
           <Link
-            to={`${import.meta.env.VITE_API_URL}/login`}
+            to={`${backURL}/login`}
             className="bg-amber-600 text-white px-6 py-3 rounded-lg text-lg hover:bg-amber-700 transition"
           >
             Login
           </Link>
           <Link
-            to={`${import.meta.env.VITE_API_URL}/register`}
+            to={`${backURL}/register`}
             className="bg-amber-600 text-white px-6 py-3 rounded-lg text-lg hover:bg-amber-700 transition"
           >
             Registro
