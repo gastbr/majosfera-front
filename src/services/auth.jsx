cf4d21fe8e9b8b252126d0973d97073f08630af5
gastbr <71907959+gastbr@users.noreply.github.com>
2025-02-24 15:22:09 +0000

    FIX axios.js, add.js, added withXSRFToken: true

--- src/services/auth.jsx
+++ src/services/auth.jsx
@@ -20,9 +20,7 @@ export const login = (email, password) => {
     return api.post('/login', { email, password },
         {
             withCredentials: true,
-            headers: {
-                'X-XSRF-TOKEN': getCookie('XSRF-TOKEN'),
-            }
+            withXSRFToken: true,
         }).then(response => {
             console.log('==> login successful:', response);
         }).catch(error => {
@@ -43,9 +41,7 @@ export const register = (name, email, password) => {
     return api.post('/register', { name, email, password },
         {
             withCredentials: true,
-            headers: {
-                'X-XSRF-TOKEN': getCookie('XSRF-TOKEN')
-            }
+            withXSRFToken: true,
         })
         .then(response => {
             console.log('==> register successful:', response);
--- src/services/axios.jsx
+++ src/services/axios.jsx
@@ -4,9 +4,7 @@ axios.defaults.withCredentials = true;
 axios.defaults.withXSRFToken = true;
 
 const api = axios.create({
-  baseURL: `${import.meta.env.VITE_API_URL}`,
-  withCredentials: true,
-  withXSRFToken: true,
+  baseURL: `${import.meta.env.VITE_API_URL}`
 });
 
 export const requestCookie = () => {
