aa51854279b5a89b72ae46ee1e0fa30c37817f36
Miriam <miriam@miriam.com>
2025-02-24 16:23:43 +0000

    ADD: CSRF cookie in axios and FIX: link axios in app

--- src/services/axios.jsx
+++ src/services/axios.jsx
@@ -1,8 +1,18 @@
 import axios from 'axios';
 
+// Función para obtener la cookie XSRF-TOKEN
+function getCookie(name) {
+  const value = `; ${document.cookie}`;
+  const parts = value.split(`; ${name}=`);
+  if (parts.length === 2) return parts.pop().split(";").shift();
+}
+
 const api = axios.create({
   baseURL: `${import.meta.env.VITE_API_URL}`,
-  withCredentials: true
+  withCredentials: true,
+  headers: {
+    'X-XSRF-TOKEN': getCookie('XSRF-TOKEN'), // 🔹 Enviar el token en cada solicitud
+  }
 });
 
 export const requestCookie = () => {
